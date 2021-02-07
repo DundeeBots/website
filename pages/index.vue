192f176f20c37e0c22b94dde81b301ba164c37b7
Joshua Lowe <joshualowe1002@gmail.com>
2021-02-07 13:46:02 +0000

    Changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="font-sans">
-    <div class="relative bg-white overflow-hidden">
-      <div class="max-w-7xl mx-auto">
+    <div class="relative overflow-hidden bg-white">
+      <div class="mx-auto max-w-7xl">
         <div
           class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
         >
           <svg
-            class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
             fill="currentColor"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
@@ -15,7 +15,7 @@
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
-          <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
             <nav
               class="relative flex items-center justify-between sm:h-10 lg:justify-start"
               aria-label="Global"
@@ -26,7 +26,7 @@
                 <div class="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
                     <span class="sr-only">DundeeBots</span>
-                    <img class="h-8 w-auto sm:h-10" src="/logo.svg" />
+                    <img class="w-auto h-8 sm:h-10" src="/logo.svg" />
                   </a>
                 </div>
               </div>
@@ -45,11 +45,11 @@
       -->
 
           <main
-            class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+            class="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
           >
             <div class="sm:text-center lg:text-left">
               <h1
-                class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+                class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
               >
                 <span class="block xl:inline">Robotics to inspire the</span>
                 <span class="block text-teal-500 xl:inline">
@@ -69,7 +69,7 @@
                 <div class="rounded-md shadow">
                   <a
                     href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
+                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
                   >
                     Learn More
                   </a>
@@ -77,7 +77,7 @@
                 <div class="mt-3 sm:mt-0 sm:ml-3">
                   <a
                     href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-teal-700 bg-teal-100 hover:bg-teal-200 md:py-4 md:text-lg md:px-10"
+                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-teal-700 bg-teal-100 border border-transparent rounded-md hover:bg-teal-200 md:py-4 md:text-lg md:px-10"
                   >
                     Book a session
                   </a>
@@ -89,7 +89,7 @@
       </div>
       <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="https://micromag.cc/content/images/size/w600/2020/10/image-32.png"
           alt=""
         />
@@ -97,21 +97,21 @@
     </div>
 
     <div
-      class="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 bg-gray-50"
+      class="relative px-4 pt-16 pb-20 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 bg-gray-50"
     >
       <div class="absolute inset-0">
         <div class="bg-gray-50 h-1/3 sm:h-2/3"></div>
       </div>
-      <div class="relative max-w-7xl mx-auto bg-gray-50">
+      <div class="relative mx-auto max-w-7xl bg-gray-50">
         <div class="text-center">
           <h2
-            class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
+            class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"
           >
             From the blog
           </h2>
         </div>
         <div
-          class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"
+          class="grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none"
         >
           <ArticleCard
             v-for="article in articles"
@@ -128,7 +128,7 @@
     </div>
 
     <div class="bg-white">
-      <div class="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
+      <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
         <div
           class="px-6 py-6 bg-teal-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center"
         >
@@ -138,12 +138,12 @@
             >
               Sign up to hear more when we launch
             </h2>
-            <p class="mt-3 max-w-3xl text-lg leading-6 text-teal-200">
+            <p class="max-w-3xl mt-3 text-lg leading-6 text-teal-200">
               Enter your email to be the first to know about the latest updates
             </p>
           </div>
           <div class="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
-            <form class="sm:flex" method="POST" data-netlify="true">
+            <form class="sm:flex" name="contact" netlify>
               <label for="emailAddress" class="sr-only">Email address</label>
               <input
                 id="emailAddress"
@@ -151,12 +151,12 @@
                 type="email"
                 autocomplete="email"
                 required
-                class="w-full border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white rounded-md"
+                class="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
                 placeholder="Enter your email"
               />
               <button
                 type="submit"
-                class="mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-white bg-teal-500 hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+                class="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-white bg-teal-500 border border-transparent rounded-md shadow hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
               >
                 Notify me
               </button>
@@ -167,50 +167,21 @@
     </div>
 
     <footer class="bg-white">
-      <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <h1 class="text-center text-base text-gray-400">
+      <div class="px-4 py-12 mx-auto overflow-hidden max-w-7xl sm:px-6 lg:px-8">
+        <h1 class="text-base text-center text-gray-400">
           Funded by the <span class="text-teal-500">IET</span>
         </h1>
-        <h1 class="mt-4 text-center text-base text-gray-400">
+        <h1 class="mt-4 text-base text-center text-gray-400">
           Hero Image by Les Pounder for micro:mag
         </h1>
-        <div class="mt-8 flex justify-center space-x-6">
-          <a href="#" class="text-gray-400 hover:text-gray-500">
-            <span class="sr-only">Facebook</span>
-            <svg
-              class="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </a>
-
-          <a href="#" class="text-gray-400 hover:text-gray-500">
-            <span class="sr-only">Instagram</span>
-            <svg
-              class="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </a>
-
-          <a href="#" class="text-gray-400 hover:text-gray-500">
+        <div class="flex justify-center mt-8 space-x-6">
+          <a
+            href="https://twitter.com/search?q=%23DundeeBots&src=typed_query"
+            class="text-gray-400 hover:text-gray-500"
+          >
             <span class="sr-only">Twitter</span>
             <svg
-              class="h-6 w-6"
+              class="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -221,10 +192,13 @@
             </svg>
           </a>
 
-          <a href="#" class="text-gray-400 hover:text-gray-500">
+          <a
+            href="https://github.com/DundeeBots"
+            class="text-gray-400 hover:text-gray-500"
+          >
             <span class="sr-only">GitHub</span>
             <svg
-              class="h-6 w-6"
+              class="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -236,8 +210,26 @@
               />
             </svg>
           </a>
+
+          <a
+            href="mailto:hello@dundeebots.uk"
+            class="text-gray-400 hover:text-gray-500"
+          >
+            <span class="sr-only">Email</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              class="w-6 h-6"
+              fill="currentColor"
+            >
+              <path
+                d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
+              />
+            </svg>
+          </a>
         </div>
-        <p class="mt-8 text-center text-base text-gray-400">
+        <p class="mt-8 text-base text-center text-gray-400">
           &copy; 2020 DundeeBots. All rights reserved.
         </p>
       </div>
