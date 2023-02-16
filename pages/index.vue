3c9e89f5d4934e038e34acff899fb8ea3d3e8345
Kerry Kidd <kerry@raspikidd.com>
2023-02-16 11:51:47 +0000

    Added Menu file

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,6 @@
   <div class="font-sans">
     <Header />
     <div class="relative overflow-hidden bg-white">
-      
-      <div class="mx-auto max-w-7xl">
-        <div
-          class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
-        >
-          <svg
-            class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-
           <!--<div class="relative px-4 pt-6 sm:px-6 lg:px-8">
             <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
               <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
@@ -41,57 +26,54 @@
           </div> -->
 
           <!--
-        Mobile menu, show/hide based on menu open state.
+          Mobile menu, show/hide based on menu open state.
 
-        Entering: "duration-150 ease-out"
+          Entering: "duration-150 ease-out"
           From: "opacity-0 scale-95"
           To: "opacity-100 scale-100"
-        Leaving: "duration-100 ease-in"
+          Leaving: "duration-100 ease-in"
           From: "opacity-100 scale-100"
           To: "opacity-0 scale-95"
-      -->
-
-          <main
-            class="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+          -->
+      <main class="mx-auto max-w-7xl">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
           >
-            <div class="sm:text-center lg:text-left">
-              <h1
-                class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
-              >
-                <span class="block xl:inline">Robotics to inspire the</span>
-                <span class="block text-teal-600 xl:inline">
-                  Next Generation</span
-                >
-              </h1>
-              <p
-                class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-              >
-                DundeeBots is a project that will explore, design and
-                manufacture educational robotic resources with the active
-                participation of young people from the local community.
-              </p>
-              <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+          <div class="sm:text-center lg:text-left">
+            <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+              <span class="block xl:inline">
+                Robotics to inspire the
+              </span>
+              <span class="block text-teal-600 xl:inline">
+                Next Generation
+              </span>
+            </h1>
+            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              DundeeBots is a project that will explore, design and manufacture educational robotic resources with the active participation 
+              of young people from the local community.
+            </p>
+            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div class="rounded-md shadow">
-                  <a href="https://dundeebots.uk/welcome"
-                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
-                  >
+                  <a href="https://dundeebots.uk/welcome" class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 md:py-4 md:text-lg md:px-10">
                     Learn More
                   </a>
                 </div>
                 <div class="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="SmartGarden"
-                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-teal-800 bg-teal-400 border border-transparent rounded-md hover:bg-teal-200 md:py-4 md:text-lg md:px-10"
-                  >
+                  <a href="SmartGarden" class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-teal-800 bg-teal-400 border border-transparent rounded-md hover:bg-teal-200 md:py-4 md:text-lg md:px-10">
                     Smart Garden
                   </a>
                 </div>
-              </div>
-              <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            </div>
+            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div class="rounded-md shadow">
-                  <a href="/HilltownStobswellTech"
-                    class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 md:py-4 md:text-lg md:px-10"
-                  >
+                  <a href="/HilltownStobswellTech" class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 md:py-4 md:text-lg md:px-10">
                     Hilltown & Stobswell
                   </a>
                 </div>
@@ -103,11 +85,11 @@
                     Smart Garden
                   </a>
                 </div> -->
-              </div>
             </div>
-          </main>
+          </div>
         </div>
-      </div>
+      </main>
+       
       <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full"
