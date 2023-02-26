7e42fa5f2b0c2d060e5dcb29bf43935d7bc2fa20
Kerry Kidd <kerry@raspikidd.com>
2023-02-26 14:09:57 +0000

    Working mobile menu

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
        <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
          <div class="flex justify-start lg:w-0 lg:flex-1">
            <a href="/">
-             <span class="sr-only">Workflow</span>
+             <span class="sr-only">DundeeBots</span>
              <img class="h-8 w-auto sm:h-20" src="@/static/logo.svg" alt="Logo">
            </a>
          </div>
